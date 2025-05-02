9a4d685a29dd2f0f2808b7315380d027a8c581fc
Darshil Vaghasiya <109270799+darshilvaghasiya@users.noreply.github.com>
2025-05-02 11:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //rest api
