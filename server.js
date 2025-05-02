88ae90ff8700853343b42d1624bdf1b097a7e926
Darshil Vaghasiya <109270799+darshilvaghasiya@users.noreply.github.com>
2025-05-02 11:59:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //configure env
 dotenv.config();
